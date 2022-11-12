8a8a476f19b262006ac1fb60465e130f8bb75608
LoliPain <p2love.loli@gmail.com>
2022-11-12 10:19:32 +0400

    basic layers

---
+++ keyboards/yandrstudio/wired/whitemouse69pro/keymaps/LoliPain/types.h
@@ -0,0 +1,22 @@
+#include QMK_KEYBOARD_H
+
+enum layer_list {
+	_QWERTY,
+	_SUPPLY,
+	_LOCKS,
+	_VIMOVEMENT,
+	_VIMOUSE,
+	_RGB,
+};
+
+enum layer_switch {
+	QWERTY = SAFE_RANGE,
+	SUPPLY,
+	LOCKS,
+	VIMOVEMNT,
+	VIMOUSE,
+	RGB
+};
+
+
+
